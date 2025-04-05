309337a242ac2490645b642d3753ea17f209739e
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2025-04-05 18:01:49 -0500

    Primer commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,13 @@ import { HomeComponent } from '../components/home/home.component';
 import { CreationalPatternsComponent } from '../components/creational-patterns/creational-patterns.component';
 import { StructuralPatternsComponent } from '../components/structural-patterns/structural-patterns.component';
 import { BehavioralPatternsComponent } from '../components/behavioral-patterns/behavioral-patterns.component';
+import { DesignPatternsComponent } from '../components/design-patterns/design-patterns.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'creational-patterns', component: CreationalPatternsComponent },
     { path: 'structural-patterns', component: StructuralPatternsComponent },
     { path: 'behavioral-patterns', component: BehavioralPatternsComponent },
+    { path: 'designpatterns', component: DesignPatternsComponent },
     { path: '**', redirectTo: '' }
   ];
