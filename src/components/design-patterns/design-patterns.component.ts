879d4e52b3a5ae29c73556ce22f23565f7e37f77
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2025-04-05 18:24:34 -0500

    Agregando carpetas

--- src/components/design-patterns/design-patterns.component.ts
+++ src/components/design-patterns/design-patterns.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-design-patterns',
-  imports: [],
+  imports: [MatCardModule],
+  standalone: true,
   templateUrl: './design-patterns.component.html',
   styleUrl: './design-patterns.component.scss'
 })
