f435df03c27524a4f9956dd8fb480924d4cdbeef
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2025-04-05 21:59:23 -0500

    Agregando cambios en tipos

--- src/components/navbar/navbar.component.ts
+++ src/components/navbar/navbar.component.ts
@@ -1,13 +1,22 @@
 import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule ],
+  imports: [RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
 
+  constructor(private location: Location, private router: Router) {}
+
+  goBack(): void {
+    const currentUrl = this.router.url;
+    if (currentUrl !== '/' && currentUrl !== '/home' && currentUrl !== '') {
+      this.location.back();
+    } 
+  } 
 }
