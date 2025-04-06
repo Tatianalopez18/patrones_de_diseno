ccb0468ad4b0a7bb7b192d138899d7613e279fc0
Tatianalopez18 <stlopez43@ucatolica.edu.co>
2025-04-05 19:11:42 -0500

    Agregando cambios en tipos

--- src/components/creational-patterns/creational-patterns.component.ts
+++ src/components/creational-patterns/creational-patterns.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import {MatCardModule} from '@angular/material/card';
 
 @Component({
   selector: 'app-creational-patterns',
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './creational-patterns.component.html',
   styleUrl: './creational-patterns.component.scss'
 })
--- src/components/design-patterns/design-patterns.component.ts
+++ src/components/design-patterns/design-patterns.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-design-patterns',
-  imports: [MatCardModule],
+  imports: [MatCardModule, RouterModule],
   standalone: true,
   templateUrl: './design-patterns.component.html',
   styleUrl: './design-patterns.component.scss'
